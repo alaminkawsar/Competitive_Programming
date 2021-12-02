fd67a277be945b3267cfe71381aeabbe82cdf3dc
kawsar <alaminkawsar8643@gmail.com>
2021-12-03 03:51:47 +0600

    File Added

---
+++ Sorting_Algorithm/Bubble_Sort.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace;
+
+class Bubble_Sort
+{
+private:
+    /* data */
+public:
+    void bubble_sort(int ar[],int n){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(ar[i]>ar[j]) swap(ar[i],ar[j]);
+            }
+        }
+    }
+};
---
+++ Sorting_Algorithm/Main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int sz = 4e5+5;
+void solve()
+{
+    
+}
+int main()
+{
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+    int t=1,ts=0; //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
